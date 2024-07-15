e2646e0b2bcb9df4816d7295c85aca3513a94f91
Imelda Hope <imeldalarusso@gmail.com>
2024-07-15 08:07:44 +0300

    creates payroll app that passes records as context

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 /* Your Code Here */
 
+function createEmployeeRecord(arr){
+    return {
+        firstName : arr[0],
+        familyName : arr[1],
+        title : arr[2],
+        payPerHour : arr[3],
+        timeInEvents : [],
+        timeOutEvents : [],
+    }
+}
+
+function createEmployeeRecords(employeesArr){
+    return employeesArr.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(timeAndDate){
+    const [date, hour] = timeAndDate.split(' ');
+
+    this.timeInEvents.push(
+        {
+            type: "TimeIn",
+            date : date,
+            hour: parseInt(hour,10)
+        }
+    )
+    
+    return this;    
+}
+
+function createTimeOutEvent(timeAndDate){
+    const [date, hour] = timeAndDate.split(' ');
+
+    this.timeOutEvents.push(
+        {
+            type: "TimeOut",
+            date: date,
+            hour: parseInt(hour,10)
+        }
+    )
+
+    return this;
+}
+
+function hoursWorkedOnDate(dateOfWork){
+    const timeInEvent = this.timeInEvents.find(emp => emp.date === dateOfWork);
+    const timeOutEvent = this.timeOutEvents.find(emp => emp.date === dateOfWork);
+
+    if (timeInEvent && timeOutEvent) {        
+        const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+        return hoursWorked;
+    } else {        
+        return 'record not found';
+    }
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(empArr, firstName){
+    return empArr.find((name) => name.firstName === firstName);
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +84,8 @@ const allWagesFor = function () {
     return payable
 }
 
+function calculatePayroll(employeesRecords){
+    return employeesRecords.reduce((tots, employeesRecords) => {
+        return tots + allWagesFor.call(employeesRecords)
+    }, 0)
+}
